3edf5076f89b6de77dcecad481f99dda0528aa61
Skyhero-alt <skyhero.code@gmail.com>
2023-06-14 09:30:37 +0530

    feat: Added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,30 @@
 import {
-  createBrowserRouter, 
+  createBrowserRouter,
   Routes,
   Route,
   Link,
   NavLink,
   createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
-import Dashboard from './pages/Dashboard';
-import { useState } from "react";
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+
 import "./App.css";
 import "./Styles/styles.css";
-import RootLayout from './Layouts/RootLayout';
-const router= createBrowserRouter(
-  createRoutesFromElements(
-    <Route  exact path="" element={<RootLayout />}>
-      <Route index element={<Dashboard />}>
-      </Route>
-    </Route>
-  )
-
-
-)
+import RootLayout from "./Layouts/RootLayout";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
+  },
+  { path: "/*", element: <Landing /> },
+]);
 
 function App() {
   const percentage = 500;
-  return (
-   <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
-
