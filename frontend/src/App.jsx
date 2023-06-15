a48da4d9902ac06d913b114136566fdfa6dbc8a5
Skyhero-alt <skyhero.code@gmail.com>
2023-06-15 22:00:16 +0530

    feat: Added register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import "./App.css";
 import "./Styles/styles.css";
 import RootLayout from "./Layouts/RootLayout";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
   },
   { path: "/land", element: <Landing /> },
   { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
 ]);
 
 function App() {
